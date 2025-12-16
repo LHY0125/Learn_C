bd04896daea132dcf1a8946d71502f9c3ca84c0e
LHY0125 <3364451258@qq.com>
2025-12-16 08:14:56 +0800

    更新

---
+++ exercise/44.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+
+// Dijkstra
+int main(void)
+{
+#ifdef _WIN32
+    system("chcp 65001 > nul");
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+#endif
+
+    
+    return 0;
+}
